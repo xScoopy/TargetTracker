6fea53034f493d2e2060460744e6aa38ae860939
xScoopy <JLeary070@gmail.com>
2020-10-14 15:29:20 -0700

    declared constants for inputs

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// declaring constants for target elements
+const white = document.querySelector('#white-target')
+const black = document.querySelector('#black-target')
+const blue = document.querySelector('#blue-target')
+const red = document.querySelector('#red-target')
+const yellow = document.querySelector('#yellow-target')
+
+// declaring display output fields
+const displayAverage = document.querySelector('#avg-display')
+const displayTotal = document.querySelector('#total-display')
+const displayMin = document.querySelector('#min-display')
+const displayMax = document.querySelector('max-display')
+
+// declaring input elements
+const inputDate = document.querySelector('#date-picker')
+const inputArrow1 = document.querySelector('#arrow1')
+const inputArrow2 = document.querySelector('#arrow2')
+const inputArrow3 = document.querySelector('#arrow3')
+const inputArrow4 = document.querySelector('#arrow4')
+const inputArrow5 = document.querySelector('#arrow5')
+
